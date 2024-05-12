7ce0421b52b30df33bc40b03df86f450a91ce89b
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-05-12 11:57:01 +0200

    reload lol

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,7 +20,7 @@ function Update-Profile {
   }
 
   try {
-    $url = "https://raw.githubusercontent.com/RoBaertschi/powershell-profile/master/Microsoft.PowerShell_profile.ps1"
+    $url = "https://raw.githubusercontent.com/RoBaertschi/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
     $oldhash = Get-FileHash $profile
     Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
     $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
@@ -30,7 +30,7 @@ function Update-Profile {
     }
   }
   catch {
-    Write-Error "Unable to check for `$profile updates"
+    Write-Error "Unable to check for `$profile updates. Error: $Error"
   }
   finally {
     Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
@@ -86,6 +86,6 @@ function uptime {
   }
 }
 
-function Update-Profile {
+function reload {
   & $profile
 }
