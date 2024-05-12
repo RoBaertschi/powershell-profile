1f97c36b5f816df9f241ac46402adc448033462d
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-05-12 12:43:47 +0200

    setup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -214,3 +214,17 @@ Set-PSReadLineOption -Colors @{
 }
 
 oh-my-posh init pwsh | Invoke-Expression
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+  Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+}
+else {
+  try {
+    winget install -e -id ajeetdsouza.zoxide
+    Write-Host "zoxide installed successfully. Initializing..." 
+    Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+  }
+  catch {
+    Write-Error "Failed to install zoxide. Error: $_"
+  }
+}
