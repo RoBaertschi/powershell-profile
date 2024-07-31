e5355d24e38e4f0e97c0785e9fd67f4695986bbe
Robin Bärtschi <72601088+RoBaertschi@users.noreply.github.com>
2024-07-31 14:10:43 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -268,9 +268,3 @@ else {
     Write-Error "Failed to install zoxide. Error: $_"
   }
 }
-
-
-#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
-
-Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
-#34de4b3d-13a8-4540-b76d-b9e8d3851756
